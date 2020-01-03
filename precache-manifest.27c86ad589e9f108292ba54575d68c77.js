bf1e1c13825ba3188e661ea24b40e79a898da638
Buckshot-77 <Buckshot-77@github.com>
2020-01-03 13:36:45 -0300

    Updates

---
+++ precache-manifest.27c86ad589e9f108292ba54575d68c77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b387d0ccbcbee8dda5c9f976a57c0ff9",
+    "url": "buckshot-77.github.io/calculadora/index.html"
+  },
+  {
+    "revision": "e911b0d943dea0a529db",
+    "url": "buckshot-77.github.io/calculadora/static/css/main.205aa3d0.chunk.css"
+  },
+  {
+    "revision": "4fa009f9e5b15b9c13b7",
+    "url": "buckshot-77.github.io/calculadora/static/js/2.43471bb6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "buckshot-77.github.io/calculadora/static/js/2.43471bb6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e911b0d943dea0a529db",
+    "url": "buckshot-77.github.io/calculadora/static/js/main.ef6c1ebd.chunk.js"
+  },
+  {
+    "revision": "92233b24a92839108361",
+    "url": "buckshot-77.github.io/calculadora/static/js/runtime-main.ddf82afa.js"
+  }
+]);
